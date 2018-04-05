28689fee93ce71343bf3a4ef2f1b6745a2ace2f1
Manjula Dube <dube.manjula668@gmail.com>
2018-04-05 22:54:40 +0530

    javascript workshop

---
+++ datatypes.js
@@ -0,0 +1,50 @@
+// 7 datatypes in javascript
+
+//Boolean
+var flag = true
+if(flag){
+  console.log("hurrah its true")
+} else {
+  console.log("hurrah its false")
+}
+
+//null
+var myNumber = null
+console.log(myNumber + 5)
+
+//null is always treated as zero
+
+//undefined
+var myNumber;
+console.log(myNumber + 5) //NAN
+
+
+
+//Number
+ var number1 = 12
+ var number2 = 13
+
+console.log(number1 + number2)
+
+//String
+
+var message = "Hello welcome to the first workshop"
+console.log(message)
+
+//Symbol
+//es6 date type instances are unique and immutable
+
+var symbol1 = Symbol('manjula')
+var symbol2 = Symbol('manjula')
+console.log(symbol1 === symbol2)
+/**convert it to <string></string> */
+console.log(String(symbol1) === String(symbol2)
+
+
+//Object
+var myObject = new Object();
+myObject.name = "Manjula"
+console.log(myObject.name)
+
+
+
---
+++ variables .js
@@ -0,0 +1,23 @@
+
+//declare the variable
+
+var myVariable1;
+
+const myVariable;
+
+let myVariable;
+
+//intialize the variable 
+
+myVariable1 = "Manjula"
+console.log(myVariable1)
+
+myVariable1 = "Tom"
+
+
+/** Dont have variables with start as number or _
+ * 
+ * Variables should be camel case, loosely type language
+ *
+ */
+
