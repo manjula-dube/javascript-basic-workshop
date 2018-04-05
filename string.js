4e07f625a29a6a0887cf5e7795289815dff84f97
Manjula Dube <dube.manjula668@gmail.com>
2018-04-05 23:53:45 +0530

    d

--- string.js
+++ string.js
@@ -4,4 +4,19 @@ console.log(myString)
 var message = "Manjula loves javascript. She says \"I am going to teach javascript today\""
 console.log(message)
 
+message += "I am concan"
+
 //conact 2 strings
+
+/**
+\' 
+\"
+\\
+\n
+\r
+\t
+\b
+\f
+
+
+ */
