5dc9ed40add7bd3be381ab23965d23eceb1ecc83
Manjula Dube <dube.manjula668@gmail.com>
2018-04-05 23:47:53 +0530

    add string

---
+++ string.js
@@ -0,0 +1,7 @@
+var myString = "Manjula" //single or double quotes
+console.log(myString)
+
+var message = "Manjula loves javascript. She says \"I am going to teach javascript today\""
+console.log(message)
+
+//conact 2 strings
