41405f7fd2ad31dd119479bbe972d43f0d81dcc7
Manjula Dube <dube.manjula668@gmail.com>
2018-04-06 10:52:54 +0530

    functions

--- function.js
+++ function.js
@@ -3,3 +3,25 @@
 function Addition(number1,number2){
   return number1 + number2 //body for the function
 }
+
+console.log(Addition(1,4)) //Call
+
+var myName = "Manjula"
+function myFunc(){
+  var myName = "Change Name"
+  console.log(myName)
+}
+
+myFunc()
+console.log(myName)
+
+///Nested function
+
+function add(num1,num2){
+  function square(num3){
+    return num3 * num3
+  }
+  return square(num1) + square(num2)
+}
+
+a = add(2,3)
