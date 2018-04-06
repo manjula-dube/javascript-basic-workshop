ddc2499b5ba9dfe0738cba97ac702fedbd5a3b64
Manjula Dube <dube.manjula668@gmail.com>
2018-04-06 11:58:30 +0530

    null v/s undefined

---
+++ null.js
@@ -0,0 +1,21 @@
+//null v/s undefined
+
+var name 
+console.log(name)
+
+
+//null is an assignment value. and if the variable is not assigned any value it will be undefined
+name = null
+console.log(name)
+
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(null === undefined)
+console.log(null == undefined)
+console.log(null === null)
+console.log(null == null)
+console.log(!null)
+console.log(!!null)
+console.log(1 + null)
+console.log(1 + undefined)
