3be5f14e305c3cca7d390ed2d9727848de214d9d
Manjula Dube <dube.manjula668@gmail.com>
2018-04-06 10:36:21 +0530

    add string methods

---
+++ string-methods.js
@@ -0,0 +1,72 @@
+var string1 = "Today we will learn javascript"
+var string2 = "We will also have some food & food"
+
+
+//charAt()
+console.log(string1.charAt(3))
+
+//concat()
+console.log(string1.concat(string2))
+
+
+//endsWith()
+console.log(string1.endsWith("javascript"))
+
+
+//fromCharCode()
+console.log(String.fromCharCode(117))
+
+//charCodeAt()
+console.log(string1.charCodeAt(3))
+
+//includes()
+console.log(string1.includes("food"))
+
+//lastIndexOf()
+console.log(string2.lastIndexOf("food"))
+
+//match
+console.log(string2.match("/food/g"))
+
+//indexOf
+console.log(string2.indexOf("food"))
+
+//repeat
+console.log(string2.indexOf("5"))
+
+//slice()
+console.log(string2.slice(2,4))
+
+//replace()
+console.log(string2.match("/food/g","Food"))
+
+//search
+console.log(string2.search("food"))
+
+//split()
+console.log(string2.split(" "))
+
+//substr()
+console.log(strign2.substr(2,4)) //this will extract the string from 2 to 4
+
+//substring()
+console.log(strign2.substring(2,4)) //like slice
+
+//toLowerCase()
+
+console.log(strign1.toLowerCase())
+
+//toUpperCase()
+
+console.log(strign1.toUpperCase())
+
+
+//trim()
+
+var string3 = "         My name is Manjula"
+console.log(strign1.trim())
+
+
+
+
+
