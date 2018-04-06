ad6387864904e8e07485f3cf867175145a6d86dc
Manjula Dube <dube.manjula668@gmail.com>
2018-04-06 16:11:35 +0530

    add object

--- objects.js
+++ objects.js
@@ -20,3 +20,29 @@ function showDetails(obj,objName){
 }
 
 console.log(showDetails(myDetails,"myDetails"))
+
+// object initializer 
+var myDetails = {name: "Manjula",age: 25,profession:"software developer"}
+
+//constructor function
+function myDetails(name,age,profession){
+  this.name = name
+  this.age = age
+  this.profession = profession
+}
+
+var mydetail = new myDetails("Mnajula",26,"Software Engineer")
+var myanotherdetail = new myDetails("Sample",26,"Software Engineer")
+console.log(myanotherdetail)
+myanotherdetail.test = "i am test"
+
+//Object.create
+var myDetails = {
+  name: "Manjula",age: 25, profession: "Software developer","displayName":function(){
+    console.log(this.name)
+  }
+}
+
+var detail =  Object.create(myDetails)
+console.log(detail.name)
+
